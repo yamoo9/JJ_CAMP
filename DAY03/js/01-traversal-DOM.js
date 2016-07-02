b4cb792020b66d7cf685d03b48ace5a7c79b76a7
Jee Hoon <yamoo9@naver.com>
2016-07-02 20:38:52 +0900

    context 추가

--- DAY03/js/01-traversal-DOM.js
+++ DAY03/js/01-traversal-DOM.js
@@ -3,25 +3,30 @@
 var toggle_btn = document.querySelector('.button-toggle');
 var container = document.querySelector('.toggled-container');
 
-// class 속성을 요소에 추가/제거하는 방법으로 공부!
-
 toggle_btn.addEventListener('click', function() {
-
-  // 조건 확인
-  var container_class = container.getAttribute('class');
-
-  if( container_class === 'toggled-container') {
-    // screen-off 클래스 속성을 추가
-    container.setAttribute('class', container_class + ' screen-off');
-  } else {
-    // console.log('screen-off를 가지고 있는 상황');
-    container.setAttribute('class', 'toggled-container');
-  }
-
-  // if (  ) {
-
+  // class 속성을 요소에 추가/제거하는 방법으로 공부!
+  // if( container.classList.contains('screen-off') ) {
+  //   container.classList.remove('screen-off');
   // } else {
-
+  //   container.classList.add('screen-off');
   // }
-
+  container.classList.toggle('screen-off');
 });
+
+/**
+ * --------------------------------
+ * DOM 탐색
+ * ----------------------------- */
+// 다음에 나오는 형제 요소노드를 찾아라.
+// .nextSibling 은 다음에 나오는(인접한) 형제 노드를 찾고,
+// .nextElementSibling 은 다음에 나오는 형제 요소노드를 찾는다.
+var toggle_btn_nextEl = toggle_btn.nextElementSibling;
+// .previousSibling 은 이전에 나오는(인접한) 형제 노드를 찾고,
+// .previousElementSibling 은 이전에 나오는 형제 요소노드를 찾는다.
+var toggle_btn_nextEl_prevEl = toggle_btn_nextEl.previousElementSibling;
+
+// toggle_btn_nextEl // <button> 다음에 나오는 <div> 요소노드
+// .previousSibling 은 이전에 나오는(인접한) 형제 노드를 찾고,
+// .previousElementSibling 은 이전에 나오는 형제 요소노드를 찾는다.
+console.log(toggle_btn_nextEl.firstElementChild.nextElementSibling.firstChild.nodeValue); // XML DOM
+console.log(toggle_btn_nextEl.firstElementChild.nextElementSibling.innerText); // HTML DOM
---
+++ DAY03/lib/dom-helper.js
@@ -0,0 +1,46 @@
+/*!
+ * dom-helper.js © yamoo9.net, 2016
+ * --------------------------------
+ * 선택, 탐색 헬퍼 함수
+ */
+
+/**
+ * CSS 선택자로 문서에서 대상(요소노드)을 찾는 헬퍼함수
+ *
+ * @method $query
+ *
+ * @작성자    yamoo9
+ * @버전     0.0.1
+ * @param  {string}  selector CSS 선택자
+ * @return {ElementNode}  요소노드
+ */
+// 복수(1개 이상), 반환 값이 노드리스트
+function $queryAll(selector, context) {
+  // 유효성 검사(Argument Validation)
+  if ( typeof selector !== 'string' ) {
+    throw new Error('전달된 인자는 선택자(문자열) 유형이 아닙니다.');
+  }
+  // 컨텍스트 객체가 존재하지 않는다면...
+  if (!context) {
+    // 컨텍스트 참조 변수에 document 객체를 참조한다.
+    context = document;
+  } else {
+    // 컨텍스트 객체가 존재한다면...
+    // 경우 2. CSS 선택자인 경우
+    if ( context.nodeType !== 1 && typeof context === 'string' ) {
+      context = $query(context);
+    }
+    // 경우 1. DOM 객체인 경우 (넘어감)
+  }
+  return context.querySelectorAll(selector);
+}
+
+// 단수(1개, 첫번째 매칭 요소)
+function $query(selector, context) {
+  return $queryAll(selector, context)[0];
+}
+
+
+// 별칭(Alias) 설정: $query, $q
+var $q = $query;
+// var $qa = $queryAll;
