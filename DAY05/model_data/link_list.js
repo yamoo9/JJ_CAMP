12e7a87b1e9cb465027b654a7026cba7ab9d4c17
Jee Hoon <yamoo9@naver.com>
2016-07-16 20:42:30 +0900

    포커스 요소인 <a> 이벤트 바인딩 (핸들러)

---
+++ DAY05/model_data/link_list.js
@@ -0,0 +1,34 @@
+// MVC => Model
+// JSON 문법
+var link_list = [
+  {
+    "name": "Fast Campus",
+    "address": "http://fastcampus.co.kr/",
+    "external": true
+  },
+  {
+    "name": "Google",
+    "address": "http://google.com/",
+    "external": false
+  },
+  {
+    "name": "NAVER",
+    "address": "http://naver.com/",
+    "external": true
+  },
+  {
+    "name": "DAUM",
+    "address": "http://daum.net/",
+    "external": false
+  },
+  {
+    "name": "Squarespace",
+    "address": "http://squarespace.com/",
+    "external": true
+  },
+  {
+    "name": "Hanwha",
+    "address": "http://hanwha.co.kr/",
+    "external": true
+  }
+];
