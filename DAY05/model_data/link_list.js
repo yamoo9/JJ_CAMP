447dd2bcc0aaba8b28bfe83f1a2286a6f880334a
Jee Hoon <yamoo9@naver.com>
2016-07-17 11:59:16 +0900

    5주차 강의 정리

--- DAY05/model_data/link_list.js
+++ DAY05/model_data/link_list.js
@@ -2,33 +2,33 @@
 // JSON 문법
 var link_list = [
   {
-    "name": "Fast Campus",
-    "address": "http://fastcampus.co.kr/",
+    "name":     "Fast Campus",
+    "address":  "http://fastcampus.co.kr/",
     "external": true
   },
   {
-    "name": "Google",
-    "address": "http://google.com/",
+    "name":     "Google",
+    "address":  "http://google.com/",
     "external": false
   },
   {
-    "name": "NAVER",
-    "address": "http://naver.com/",
+    "name":     "NAVER",
+    "address":  "http://naver.com/",
     "external": true
   },
   {
-    "name": "DAUM",
-    "address": "http://daum.net/",
+    "name":     "DAUM",
+    "address":  "http://daum.net/",
     "external": false
   },
   {
-    "name": "Squarespace",
-    "address": "http://squarespace.com/",
+    "name":     "Squarespace",
+    "address":  "http://squarespace.com/",
     "external": true
   },
   {
-    "name": "Hanwha",
-    "address": "http://hanwha.co.kr/",
+    "name":     "Hanwha",
+    "address":  "http://hanwha.co.kr/",
     "external": true
   }
 ];
