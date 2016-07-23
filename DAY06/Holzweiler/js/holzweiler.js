e23fc5fecf3917176948315d3d1e57a663f03c4e
Jee Hoon <yamoo9@naver.com>
2016-07-23 16:58:26 +0900

    6일차 수업 준비

---
+++ DAY06/Holzweiler/js/holzweiler.js
@@ -0,0 +1,71 @@
+/*! holzweiler.js © yamoo9.net, 2016 */
+
+/**
+ * ----------------------------------------------------------------------
+ * 내비게이션 컨트롤 (절차 지향 방식, 함수형 프로그래밍)
+ * ------------------------------------------------------------------- */
+
+/**
+ * -----------------------------------------------------------------
+ * 내비게이션 접근성 설정 함수
+ */
+// 하위 메뉴 펼침 함수
+function activateDepth2Menu() {
+  var _parent_li = this.parentNode;
+  deactiveDepth2Menu(_parent_li);
+  _parent_li.classList.add('active');
+}
+// 하위 메뉴 접힘 함수
+function deactiveDepth2Menu(_parent_li) {
+  var _parent_ul    = _parent_li.parentNode;
+  var _activated_el = $query('.active', _parent_ul);
+  if ( _activated_el ) { _activated_el.classList.remove('active'); }
+}
+// Blur 이벤트 감지 시, 하위 메뉴 접힘 함수
+function blurDeactiveDepth2Menu() {
+  var _d1_parent_li = this.parentNode.parentNode.parentNode;
+  deactiveDepth2Menu(_d1_parent_li);
+}
+// 키보드 Shift + Tab 감지 시, 하위 메뉴 접힘 함수
+function backTabDeactiveDepth2Menu(event) {
+  var _key = event.keyCode || event.which;
+  // Tab 키 식별 숫자는 9 이다.
+  if(event.shiftKey && _key === 9) { deactiveDepth2Menu(this.parentNode); }
+}
+
+/**
+ * -----------------------------------------------------------------
+ * 내비게이션 접근성 설정 초기화 (Initialization)
+ */
+var gnb          = $query('.holzweiler-gnb');
+var unb          = $query('.holzweiler-unb');
+var gnb_d1_links = $queryAll('.depth-1 > li > a', gnb);
+var unb_d1_links = $queryAll('.depth-1 > li > a', unb);
+
+var i=0, l=gnb_d1_links.length, gnb_link, unb_link, gnb_last_a, unb_last_a;
+
+/**
+ * -----------------------------------------------------------------
+ * 내비게이션 접근성 설정 이벤트 바인딩
+ * 요소에 개별적 이벤트 적용을 위한 반복문 사용.
+ * ※ 자바스크립트 호이스트 현상 고려, for문의 변수 선언은 초기화 영역으로 이동 정리.
+ */
+for(; i<l; i++) {
+  gnb_link = gnb_d1_links[i];
+  unb_link = unb_d1_links[i];
+  // Depth 1 <a> 요소에 포커싱될 경우, 하위 메뉴 펼쳐짐
+  gnb_link.addEventListener('focus', activateDepth2Menu);
+  unb_link.addEventListener('focus', activateDepth2Menu);
+  // Depth 1 <a> 첫번째 요소에서 shift + tab 키를 누를 경우, 하위 메뉴 닫힘
+  if ( i === 0 ) {
+    gnb_link.addEventListener('keydown', backTabDeactiveDepth2Menu);
+    unb_link.addEventListener('keydown', backTabDeactiveDepth2Menu);
+  }
+  // Depth 2 마지막 <a> 요소에서 blur 이벤트 발생 시, 하위 메뉴 닫힘
+  if ( i === l - 1 ) {
+    gnb_last_a = $query('.depth-2 li:last-child a', gnb_link.parentNode);
+    unb_last_a = $query('.depth-2 li:last-child a', unb_link.parentNode);
+    gnb_last_a.addEventListener('blur', blurDeactiveDepth2Menu);
+    unb_last_a.addEventListener('blur', blurDeactiveDepth2Menu);
+  }
+}
---
+++ DAY06/README.md
@@ -0,0 +1,61 @@
+###### Fast Campus
+
+## [JavaScript & jQuery 정복 CAMP](http://www.fastcampus.co.kr/dev_camp_jst/)
+
+# DAY06
+
+### 문서객체모델(DOM) **조작**
+
+- 문서 객체 조작
+  - 속성 가져오기/설정하기/소유확인/제거하기
+  - 추가(삽입)
+  - 이동
+  - 복제
+  - 제거
+
+-
+
+### 탐색과 관련한 헬퍼 함수 만들기
+
+- `nextEl()`
+- `prevEl()`
+- `firstEl()`
+- `lastEl()`
+- `parentEl()`
+
+-
+
+### 조작과 관련한 헬퍼 함수 만들기
+
+- `getAttr()`
+- `setAttr()`
+- `hasAttr()`
+- `attr()`
+- `removeAttr()`
+
+- `append()`
+- `appendTo()`
+- `prepend()`
+- `prependTo()`
+
+- `html()`
+- `text()`
+
+- `addClass()`
+- `removeClass()`
+- `hasClass()`
+- `toggleClass()`
+- `radioClass()`
+
+-
+
+### JavaScript 객체 정복
+
+- 생성자와 프로토타입 객체
+- 리터럴 표현식
+- 객체 인스턴스 유형 체크
+- 정확한 값을 반환하는 객체 판별 헬퍼 함수 만들기
+– 내장 생성자와 프로토타입 멤버
+– 공인된 내장 프로토타입 확장
+– Object, Function 객체
+– 함수, 스코프, 컨텍스트, 호이스트, 클로저
