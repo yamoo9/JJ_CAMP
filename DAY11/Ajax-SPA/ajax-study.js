71b05db0e5bb8f5dc83977cbc9cc71ee0db54d64
Jee Hoon <yamoo9@naver.com>
2016-08-27 20:55:24 +0900

    Ajax 준비

---
+++ DAY11/Ajax-SPA/ajax-study.js
@@ -0,0 +1,10 @@
+/*! ajax-study.js © yamoo9.net, 2016 */
+(function(global, XHR){
+  'use strict';
+
+  // Ajax (비동기 통신)를 수행할 객체 생성
+  var xhr = new XHR();
+  // 생성된 XHR 객체 검토
+  console.dir(xhr);
+
+})(this, this.XMLHttpRequest);
