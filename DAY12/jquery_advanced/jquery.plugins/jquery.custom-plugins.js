71ee45107f4987e1b6a4881137a6d731919831f5
Jee Hoon <yamoo9@naver.com>
2016-09-03 22:08:05 +0900

    jQuery 플러그인 제작 시 유의해야 할 점

---
+++ DAY12/jquery_advanced/jquery.plugins/jquery.custom-plugins.js
@@ -0,0 +1,19 @@
+(function(global, $){
+  'use strict';
+
+  $.fn.extend({
+    'focusToEnd': function() {
+      var $this = this;
+      return $.each($this, function(idx, el){
+        var $_this = $this.eq(idx);
+      });
+    },
+    'carousel': function() {
+
+    },
+    'tab': function() {
+
+    }
+  });
+
+})(this, this.jQuery);
