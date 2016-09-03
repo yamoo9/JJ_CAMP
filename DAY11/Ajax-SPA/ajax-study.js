65b6383bfa9922ae307dfc898ea77c6691463d66
Jee Hoon <yamoo9@naver.com>
2016-09-03 17:50:06 +0900

    Ajax 통신을 위한 Settings 객체 생성

--- DAY11/Ajax-SPA/ajax-study.js
+++ DAY11/Ajax-SPA/ajax-study.js
@@ -14,8 +14,8 @@
   // xhr.open('GET', 'data/data.html');
   // xhr.open('GET', 'data/data.xml');
   xhr.open('GET', 'data/data.json');
-  // console.log('xhr.send:', !!xhr.send);
-  // xhr.send();
+  console.log('xhr.send:', !!xhr.send);
+  xhr.send();
 
   // 비동기 통신을 위한 이벤트 구문 작성
   xhr.onreadystatechange = function() {
