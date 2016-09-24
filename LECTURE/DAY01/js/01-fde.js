38413ac1077558576d071f1596b83e2f49cb6d5c
Jee Hoon <yamoo9@naver.com>
2016-09-24 18:40:35 +0900

    돔 스크립트 생성/삽입

--- LECTURE/DAY01/js/01-fde.js
+++ LECTURE/DAY01/js/01-fde.js
@@ -4,10 +4,30 @@
 function initialization() {
   // 문서에서 <p> 요소를 찾아 변수에 참조
   // 문서에서 첫번째 <p> 요소를 찾아온다.
-  var target_p;
-  target_p = document.getElementsByTagName('p').item(0);
+  var target_p = document.getElementsByTagName('p').item(0);
+  // console.log(target_p);
 
-  console.log(target_p);
+  // 단계 1.
+  // <h1> 요소를 생성하고,
+  var headline = document.createElement('h1');
+  // 텍스트 내용으로 `JavaScript Log` 라고하는 텍스트를 동적으로 생성한다.
+  var headline_content = document.createTextNode('JavaScript Log');
+  // 생성된 각 노드(Node) 검증
+  // console.log('headline:', headline);
+  // console.log('headline_content:', headline_content);
+  // 각 노드를 합치기(둘 중 하나는 부모 노드, 자식 노드가 되어야 함)
+
+  // 단계 2.
+  // DOM API: ~ 자식으로 삽입
+  // 부모노드.appendChild(자식노드)
+  headline.appendChild(headline_content);
+  // 합쳐진 노드 결과 검증
+  // console.log('headline:', headline);
+
+  // 단계 3.
+  // DOM API: ~ 앞에 삽입
+  // 목표노드.부모노드.insertBefore(삽입노드, 목표노드);
+  target_p.parentNode.insertBefore(headline, target_p);
 
 }
 
