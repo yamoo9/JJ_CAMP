0acab20bfd93d1fa0bf91a97b5e490bcfab32733
Jee Hoon <yamoo9@naver.com>
2016-09-24 19:11:19 +0900

    비순차 목록 동적으로 문서에 추가하는 코드

--- LECTURE/DAY01/js/01-fde.js
+++ LECTURE/DAY01/js/01-fde.js
@@ -2,6 +2,8 @@
 
 /** @function initialization */
 function initialization() {
+  // var body = document.getElementsByTagName('body').item(0); // XML DOM 방식
+  var body = document.body; // HTML DOM 방식
   // 문서에서 <p> 요소를 찾아 변수에 참조
   // 문서에서 첫번째 <p> 요소를 찾아온다.
   var target_p = document.getElementsByTagName('p').item(0);
@@ -28,6 +30,55 @@ function initialization() {
   // DOM API: ~ 앞에 삽입
   // 목표노드.부모노드.insertBefore(삽입노드, 목표노드);
   target_p.parentNode.insertBefore(headline, target_p);
+
+  /**
+   * --------------------------------
+   * ul 생성
+   * li 생성 x3
+   * 콘텐츠 생성 x3
+   * li + 콘텐츠 접합 x3
+   * ul + li x3 접합
+   * ul > target_p 뒤에 삽입
+   */
+
+  // <ul>
+  //   <li>IOT</li>
+  //   <li>VR</li>
+  //   <li>IT</li>
+  // </ul>
+
+  var categories = 'IOT VR IT'.split(' ');
+  // console.log(categories);
+
+  // Legacy 방법
+  // var a = 0, l = categories.length;
+  // for( ; a<l; a=a+1 ) {
+  //   console.log( categories[a], a );
+  // }
+
+  // 크로스 브라우징 이슈: ES5 Shim JS Library
+  // Modern 방법
+
+  var ul = document.createElement('ul');
+
+  categories.forEach(function(item, index) {
+    // console.log(item, index);
+    // <li>item</li>
+    var li = document.createElement('li');
+    var li_content = document.createTextNode(item);
+    li.appendChild(li_content);
+    // <ul> 요소 내부에 삽입
+    ul.appendChild(li);
+  });
+
+  // console.log(ul);
+  body.appendChild(ul);
+
+  // ES2015
+  // for (category of categories) {
+  //   console.log(category);
+  // }
+
 
 }
 
