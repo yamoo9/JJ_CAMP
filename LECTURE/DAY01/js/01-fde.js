2aa71c0c58aa5cc376877d72e0032ada04c57ea3
Jee Hoon <yamoo9@naver.com>
2016-09-24 19:42:45 +0900

    createHeadline 함수 작성

--- LECTURE/DAY01/js/01-fde.js
+++ LECTURE/DAY01/js/01-fde.js
@@ -9,27 +9,10 @@ function initialization() {
   var target_p = document.getElementsByTagName('p').item(0);
   // console.log(target_p);
 
-  // 단계 1.
-  // <h1> 요소를 생성하고,
-  var headline = document.createElement('h1');
-  // 텍스트 내용으로 `JavaScript Log` 라고하는 텍스트를 동적으로 생성한다.
-  var headline_content = document.createTextNode('JavaScript Log');
-  // 생성된 각 노드(Node) 검증
-  // console.log('headline:', headline);
-  // console.log('headline_content:', headline_content);
-  // 각 노드를 합치기(둘 중 하나는 부모 노드, 자식 노드가 되어야 함)
-
-  // 단계 2.
-  // DOM API: ~ 자식으로 삽입
-  // 부모노드.appendChild(자식노드)
-  headline.appendChild(headline_content);
-  // 합쳐진 노드 결과 검증
-  // console.log('headline:', headline);
-
-  // 단계 3.
-  // DOM API: ~ 앞에 삽입
-  // 목표노드.부모노드.insertBefore(삽입노드, 목표노드);
-  target_p.parentNode.insertBefore(headline, target_p);
+  // 호출할 함수
+  window.setTimeout(function() {
+    createHeadline('h1', 'JavaScript Log', target_p);
+  }, 2000);
 
   /**
    * --------------------------------
@@ -80,6 +63,38 @@ function initialization() {
   // }
 
 
+}
+
+/** @function createHeadline */
+function createHeadline(h_lv, content, target) {
+  // 유효성 검사
+  if( typeof h_lv !== 'string' ) { throw new Error('첫번째 인자는 문자열이어야 한다.'); }
+  if( typeof content !== 'string' ) { throw new Error('두번째 인자도 문자열이어야 한다.'); }
+  if( target && target.nodeType !== 1 ) { throw new Error('세번째 인자는 요소노드여야 한다.'); }
+  // 단계 1.
+  // <h1> 요소를 생성하고,
+  var headline = document.createElement(h_lv);
+  // 텍스트 내용으로 `JavaScript Log` 라고하는 텍스트를 동적으로 생성한다.
+  var headline_content = document.createTextNode(content);
+  // 생성된 각 노드(Node) 검증
+  // console.log('headline:', headline);
+  // console.log('headline_content:', headline_content);
+  // 각 노드를 합치기(둘 중 하나는 부모 노드, 자식 노드가 되어야 함)
+
+  // 단계 2.
+  // DOM API: ~ 자식으로 삽입
+  // 부모노드.appendChild(자식노드)
+  headline.appendChild(headline_content);
+  // 합쳐진 노드 결과 검증
+  // console.log('headline:', headline);
+
+  // 단계 3.
+  // DOM API: ~ 앞에 삽입
+  // 목표노드.부모노드.insertBefore(삽입노드, 목표노드);
+  if ( target ) {
+    target.parentNode.insertBefore(headline, target);
+  }
+  return headline;
 }
 
 // window.alert('excute javascript code');
