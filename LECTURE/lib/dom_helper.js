fdbcc4f56e8249bb795f8bd89c629b896fdd0f7f
Jee Hoon <yamoo9@naver.com>
2016-10-16 21:07:06 +0900

    dom_helper.js 압축

--- LECTURE/lib/dom_helper.js
+++ LECTURE/lib/dom_helper.js
@@ -1,3 +1,5 @@
+/*! dom_helper.js © yamoo9.net, 2016 */
+
 /**
  * --------------------------------
  * Utility Helper Functions
@@ -47,6 +49,10 @@ function isElement(node) {
   if (!node) { return false; }
   return node.nodeType === 1;
 }
+/** @function isDocument() */
+function isDocument(node) {
+  return node.nodeType === 9;
+}
 /** @function validate() */
 // 조건 확인 후, 조건이 참이면 오류 메시지를 띄움과 동시에 코드를 정지시킴.
 function validate(condition, error_message) {
@@ -64,10 +70,10 @@ function isValidate(condition, success, fail) {
 /** @function detectFeature() */
 function detectFeature(property) {
   validate( !isString(property), '첫번째 인자는 문자 유형이어야 합니다.' );
-  return property in detectFeature.dummy.style;
+  return property in detectFeature.style;
 }
 // 메모이제이션 패턴
-detectFeature.dummy = document.createElement('div');
+detectFeature.style = document.createElement('div').style;
 
 // -----------------------------
 // 함수 표현식 + 클로저
@@ -83,13 +89,13 @@ var detectFeatures = (function(){
   // 클로저 함수
   function _detectFetures(properties, element) {
     // 한 줄(인라인)로 코드를 작성하는 것을 선호하는 사람들
-    // el = ((element && isElement(element)) && element) || root_element;
+    el = ((element && isElement(element)) && element) || root_element;
     // 정석대로 조건 구문을 사용한다면 아래처럼_
-    if ( element && isElement(element) ) {
-      el = element;
-    } else {
-      el = root_element;
-    }
+    // if ( element && isElement(element) ) {
+    //   el = element;
+    // } else {
+    //   el = root_element;
+    // }
 
     validate( !isArray(properties), 'properties는 배열 유형이어야 합니다.' );
     for( var i=properties.length; properties[--i]; ) {
@@ -103,23 +109,23 @@ var detectFeatures = (function(){
 
 // -----------------------------
 // 함수 선언식 + 메모이제이션 패턴
-  // function detectFeatures(properties, element) {
-  //   detectFeatures.element = ((element && isElement(element)) && element) || detectFeatures.root_element;
-  //   validate( !isArray(properties), 'properties는 배열 유형이어야 합니다.' );
-  //   for( var property, i=properties.length; (property = properties[--i]); ) {
-  //     detectFeatures.property = property;
-  //     isValidate( detectFeature(property), detectFeatures.success, detectFeatures.fail );
-  //   }
-  // }
-  // detectFeatures.element = null;
-  // detectFeatures.property = null;
-  // detectFeatures.root_element = document.documentElement; // <html>
-  // detectFeatures.success = function(){
-  //   detectFeatures.element.classList.add(detectFeatures.property);
-  // };
-  // detectFeatures.fail = function(){
-  //   detectFeatures.element.classList.add('no-' + detectFeatures.property);
-  // };
+// function detectFeatures(properties, element) {
+//   detectFeatures.element = ((element && isElement(element)) && element) || detectFeatures.root_element;
+//   validate( !isArray(properties), 'properties는 배열 유형이어야 합니다.' );
+//   for( var property, i=properties.length; (property = properties[--i]); ) {
+//     detectFeatures.property = property;
+//     isValidate( detectFeature(property), detectFeatures.success, detectFeatures.fail );
+//   }
+// }
+// detectFeatures.element = null;
+// detectFeatures.property = null;
+// detectFeatures.root_element = document.documentElement; // <html>
+// detectFeatures.success = function(){
+//   detectFeatures.element.classList.add(detectFeatures.property);
+// };
+// detectFeatures.fail = function(){
+//   detectFeatures.element.classList.add('no-' + detectFeatures.property);
+// };
 
 
 /**
@@ -130,14 +136,16 @@ var detectFeatures = (function(){
 /** @function id() */
 function id(name) {
   // 타입 검증
-  validate(typeof name !== 'string', '전달된 인자는 문자 유형이어야만 합니다.');
+  // validate(typeof name !== 'string', '전달된 인자는 문자 유형이어야만 합니다.');
+  validate(!isString(name), '전달된 인자는 문자 유형이어야만 합니다.');
   return document.getElementById(name);
 }
 
 /** @function tag() */
 function tag(name, context) {
   // 타입 검증
-  validate(typeof name !== 'string', '전달된 인자는 문자 유형이어야만 합니다.');
+  // validate(typeof name !== 'string', '전달된 인자는 문자 유형이어야만 합니다.');
+  validate(!isString(name), '전달된 인자는 문자 유형이어야만 합니다.');
   validate( context && !isElement(context), 'context 객체는 문서 요소 객체여야만 합니다.' );
   // if ( context && context.nodeType !== document.ELEMENT_NODE ) {
   //   throw new Error('context 객체는 문서 요소 객체여야만 합니다.');
@@ -166,20 +174,19 @@ var classEls = (function(){
   } else {
     _classEls = function(name, context) {
       validate(!isString(name), 'name 인자는 문자열이어야 합니다.');
-      var all_els = tag('*', ((context && isElement(context)) && context) || document.body);
-      var all_els_length = all_els.length; // 6
-      var el = null;
-      var class_name = '';
-      var filtered_els = [];
+      var all_els        = tag('*', ((context && isElement(context)) && context) || document.body);
+      var all_els_length = all_els.length;
+      var el             = null;
+      var class_name     = '';
+      var filtered_els   = [];
       // 정규 표현식 사용
       // ^    시작 값을 검증
       // $    끝나는 값을 검증
       // \s   공백을 검증
       // +    1개 이상
       // ※ new RegExp() 사용 시에는 문자열 내부의 \s 사용 시, Escape 처리를 해야 한다.
-      var reg = new RegExp('(^|\\s)+' + name + '(\\s|$)+');
-      while(all_els_length--) { // 6, 5, 4, 3, 2, 1, 0
-        // 5, 4, 3, 2, 1, 0
+      var reg            = new RegExp('(^|\\s)+' + name + '(\\s|$)+');
+      while(all_els_length--) {
         el = all_els[all_els_length];
         class_name = el.getAttribute('class');
         if( reg.test(class_name) ) {
@@ -189,33 +196,24 @@ var classEls = (function(){
       return filtered_els;
     };
   }
-
   return _classEls;
 }());
 
 /** @function queryAll() */
 function queryAll(selector, context ) {
   // 첫번째 전달인자(Argument)의 유효성 검사
-  var _ex = function(){
-    console.info('전달인자는 문자열로 전달해야 합니다.');
-    return null;
-  };
-
-  isValidate(typeof selector !== 'string', _ex)
+  validate( !isString(selector), '전달인자는 문자열로 전달해야 합니다.' );
 
   // 사용자 정의 값이 없을 경우, 값을 초기화
-  context = context || document;
+  context = (isString(context) ? query(context) : context) || document;
   // if ( !context ) {
   //   context = document;
   // }
   // 두번째 전달인자(Argument)의 유효성 검사
-  if ( typeof context === 'string' ) {
-    context = query(context);
-  }
-  validate(
-    context.nodeType !== 1 && context.nodeType !== 9,
-    '두번째 전달인자는 요소노드여야 합니다.'
-  );
+  // if ( isString(context) ) {
+  //   context = query(context);
+  // }
+  validate( !isElement(context) && !isDocument(context), '두번째 전달인자는 요소노드여야 합니다.' );
   return context.querySelectorAll(selector); // Nodelist []
 }
 
@@ -251,9 +249,12 @@ function createEl(node_name, properties) {
 function attrs(element, properties) {
   validate(!isElement(element), 'element는 요소노드여야 합니다.');
   validate(!isObject(properties), 'properties는 객체 유형이어야 합니다.');
+  if (isEmptyObject(properties)) { console.info('설정할 속성이 존재하지 않습니다.'); return; }
   // 객체 순환
   for ( var prop in properties) {
-    var value = properties[prop];
-    element.setAttribute(prop, value);
+    if ( properties.hasOwnProperty(prop) ) {
+      var value = properties[prop];
+      element.setAttribute(prop, value);
+    }
   }
 }
