4e1b834b6c3b1e3271483d040f94af28b36adcae
Jee Hoon <yamoo9@naver.com>
2016-11-12 19:51:57 +0900

    radioClass 추가

--- LECTURE/DAY08/like-jquery/Element.prototype.js
+++ LECTURE/DAY08/like-jquery/Element.prototype.js
@@ -1,5 +1,41 @@
-if ( !('addClass' in Element.prototype) ) {
-  Element.prototype.addClass = function(name) {
+(function(global, $){
+
+  'use strict';
+
+  var old_this = null;
+
+  $.radioClass = function(name) {
+    // 기존에 name 변수에 복사된 문자 값을 class 속성으로 가진 요소에서는 해당 클래스 속성을 제거해야 한다.
+    if ( old_this !== null ) {
+      old_this.removeClass(name);
+    }
+    // ------------------------------------------------
+    this.addClass(name);
+    old_this = this;
+    // ------------------------------------------------
+    return this;
+  };
+
+  $.hasClass = function(name) {
+    // ES 3
+    var has_check_class = new RegExp( '(^|\\s+)' + name + '(\\s+|$)' );
+    // ES 6 (2015)
+    // String Template
+    // var has_check_class = new RegExp( `(^|\\s+)${name}(\\s+|$)` );
+    return has_check_class.test( this.getAttribute('class') );
+  };
+
+  $.toggleClass = function(name) {
+    if ( this.hasClass(name) ) {
+      this.removeClass(name);
+    } else {
+      this.addClass(name);
+    }
+    return this;
+    // return this.hasClass(name) ? this.removeClass(name) : this.addClass(name);
+  };
+
+  $.addClass = function(name) {
     // this // DOM ElementNode
     name = name.split(' ');
     for(var i=0, l=name.length; i<l; i++) {
@@ -8,7 +44,7 @@ if ( !('addClass' in Element.prototype) ) {
     return this;
   };
 
-  Element.prototype.removeClass = function(name) {
+  $.removeClass = function(name) {
     name = name || '*';
     if ( name === '*' ) {
       this.className = '';
@@ -19,4 +55,5 @@ if ( !('addClass' in Element.prototype) ) {
       this.classList.remove(name[i]);
     }
   };
-}
+
+})(this, Element.prototype);
