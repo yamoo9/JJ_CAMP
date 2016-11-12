96381041b19064310ab746daaba6a99f408e5266
Jee Hoon <yamoo9@naver.com>
2016-11-12 21:54:55 +0900

    Ajax 통신을 사용한 템플릿 코드 출력

--- LECTURE/DAY08/Ajax-SPA/ajax-study.js
+++ LECTURE/DAY08/Ajax-SPA/ajax-study.js
@@ -1,66 +1,89 @@
 /*! ajax-study.js © yamoo9.net, 2016 */
-(function(global, XHR){
+(function(global, Ajax){
   'use strict';
 
-  // Ajax (비동기 통신)를 수행할 객체 생성
-  var xhr = new XHR();
-  // 생성된 XHR 객체 검토
-  // console.log('xhr.open:', !!xhr.open);
-  // 동기(Syncronous)적으로 통신하는 방법 사용 [권장 X]
-  // xhr.open('GET', 'data/data.txt', false);
-  // 비동기(Asyncronous) 통신 방법
-  // xhr.open('GET', 'data/data.txt', true);
-  // xhr.open('GET', 'data/data.txt');
-  // xhr.open('GET', 'data/data.html');
-  xhr.open('GET', 'data/data.xaml');
-  // xhr.open('GET', 'data/data.json');
-  // console.log('xhr.send:', !!xhr.send);
-  // xhr.send();
+  // 비동기 통신을 하기 위한 객체 생성
+  // 생성자 함수 XMLHttpRequest 를 통해
+  var xhr = new Ajax();
+  // console.dir(xhr);
 
-  // 비동기 통신을 위한 이벤트 구문 작성
+  // XMLHttpRequest.prototype
+  // xhr.constructor.prototype
+  // xhr.__proto__
+
+  // false: 동기 방식의 통신
+  // xhr.open('GET', './data/data.txt', false);
+
+  // true:  비동기 방식의 통신
+  // xhr.open('GET', './data/data.txt', true);
+  // xhr.open('GET', './data/data.txt');
+  // xhr.open('GET', './data/data.html');
+  // xhr.open('GET', './data/data.xml');
+  xhr.open('GET', './data/data.json');
+
+  // xhr.send(); // 서버에 요청 응답을 받을 때 아무 것도 안함
+
+  // 전송 상태 확인
+  // if ( xhr.status === 200 ) {
+  //   console.log('success');
+  //   console.log(xhr.responseText); // 전송 성공 시, 응답 받은 데이터
+  // } else {
+  //   console.log(xhr.status);
+  //   console.info('fail');
+  // }
+
+  // 비동기 통신 이벤트 감지 구문
   xhr.onreadystatechange = function() {
-    console.log(this);
-    if ( this.status === 200 && this.readyState === 4 ) {
-      // TEXT 요청
-      // var data = this.response.split('.');
-      // var template_html = [];
-      // for ( var text, i=0, l=data.length; i<l; i++ ) {
-      //   text = data[i];
-      //   if ( text.trim() !== '' ) {
-      //     template_html.push( '<p>' + text + '.</p>' );
-      //   }
+    if ( xhr.status === 200 && xhr.readyState === 4 ) {
+      console.log('success');
+      // console.log(xhr.responseText); // 전송 성공 시, 응답 받은 데이터
+      // console.log(xhr.response); // 전송 성공 시, 응답 받은 데이터
+      // console.log(xhr.responseXML); // 전송 성공 시, 응답 받은 데이터
+      // ajax_container.innerHTML = xhr.responseText;
+      // ajax_container.innerHTML = xhr.responseText;
+      // var xml_doc = xhr.responseXML;
+      // global.xml_doc = xml_doc;
+      // var xml_doc_root = xml_doc.getElementsByTagName('user');
+      // var xml_doc_results = xml_doc_root[0].getElementsByTagName('results');
+      // for ( var i=0, l=xml_doc_results.length; i<l; i++ ) {
+      //   console.log(xml_doc_results[i]);
       // }
-      // template_html = template_html.join('');
-      // container.innerHTML = template_html;
-      // --------------------------------------------------------------
-      // HTML 요청
-      // container.innerHTML = this.response;
-      // --------------------------------------------------------------
-      // XML 요청
-      // var xmlDoc = this.responseXML;
-      // var xmlDoc_root = xmlDoc.firstChild;
-      // console.log(xmlDoc_root.children.length);
-      // // 힘들어 ㅠㅡㅠ 포기....
-      // --------------------------------------------------------------
-      // JSON 요청
-      var people = JSON.parse(this.response).results;
-      for (var person of people) {
-        console.log(person.email);
-      }
+      // ajax_container.innerHTML = xhr.responseXML;
+
+      // JSON
+      var Data_json2obj = global.JSON.parse(xhr.response);
+      var html_template = '';
+      [].forEach.call(Data_json2obj.results, function(item) {
+        html_template += '<ul class="list">';
+        html_template +=  `<li><img class="fl" src="${item.picture.thumbnail}" alt=""></li>`;
+        html_template +=  `<li><h4>${item.gender}</h4></li>`;
+        html_template +=  `<li><h2 class="user-name">${item.name.title}, ${item.name.first} ${item.name.last}</h2></li>`;
+        html_template +=  `<li>${item.location.street}</li>`;
+        html_template += '</ul>';
+      });
+
+      ajax_container.innerHTML = html_template;
+
     } else {
-      container_p.innerHTML = '아무래도... 통신에 실패한 듯 합니다. ㅠㅡㅠ';
+      // console.info('데이터 요청 중입니다.... ');
+      ajax_container.innerHTML = '데이터 요청 중입니다.... ';
     }
-  };
+  }
 
-  // Ajax 통신으로 가져온 데이터를 뿌릴 컨테이너 요소
-  var call_ajax_btn = document.querySelector('.call-ajax-btn');
-  var container = document.querySelector('.ajax-container');
-  var container_p = container.querySelector('p');
+  var ajax_call_btn  = document.querySelector('.call-ajax-btn');
+  var ajax_container = document.querySelector('.ajax-container');
 
-  call_ajax_btn.addEventListener('click', function() {
+  ajax_call_btn.addEventListener('click', function() {
     xhr.send();
+    // 동기 방식 (기다리다 데이터가 전송되면 아래 조건문 실행 ..................)
+    // 비동기 방식 (기다리지 않고 아래 조건문이 실행 ..................)
+    // if ( xhr.status === 200 ) {
+    //   console.log('success');
+    //   console.log(xhr.responseText); // 전송 성공 시, 응답 받은 데이터
+    //   ajax_container.innerHTML = xhr.responseText;
+    // } else {
+    //   console.log('기다리지 않겠어!!! ');
+    // }
   });
-
-
 
 })(this, this.XMLHttpRequest);
