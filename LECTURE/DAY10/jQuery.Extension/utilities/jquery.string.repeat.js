a24a56fd060e6380cc0a470b11d55fe866efa133
Jee Hoon <yamoo9@naver.com>
2016-11-26 18:01:01 +0900

    jQuery repeat() 유틸리티 메소드

--- LECTURE/DAY10/jQuery.Extension/utilities/jquery.string.repeat.js
+++ LECTURE/DAY10/jQuery.Extension/utilities/jquery.string.repeat.js
@@ -2,5 +2,30 @@
 (function(global, $){
   'use strict';
 
+  // ES6 추가된 메소드 String.prototype.repeat
+  // 'yamoo9 '.repeat(3); // 'yamoo9 yamoo9 yamoo9 '
+
+  var repeat = function(str, repeat_count) {
+    // 전달 받은 문자를 repeat_count 만큼 반복하여 생성한 문자를 반환
+    // 초기 값 설정
+    repeat_count = repeat_count || 1;
+    // 검증
+    if ( $.type(str) !== 'string' ) {
+      throw new Error('첫번째 인자는 문자열로!');
+    }
+    if ( $.type(repeat_count) !== 'number' ) {
+      throw new Error('첫번째 인자는 숫자로!');
+    }
+    var processing_str = '';
+    // 반복 순환하여 문자열을 접합 로직
+    while ( repeat_count-- ) {
+      processing_str += str;
+    }
+    return processing_str;
+  };
+
+  if ( !$.repeat ) {
+    $.repeat = repeat;
+  }
 
 })(this, this.jQuery);
